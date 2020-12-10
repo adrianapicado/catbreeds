b18c1a73807f6e1b67fd62234e2e018eaa7932a6
adriana <picadoadriana7@gmail.com>
2020-12-10 17:25:47 -0500

    added features

--- lib/cli.rb
+++ lib/cli.rb
@@ -22,8 +22,10 @@ EOF
      puts ""
      puts ""
      puts "Welcome User!"
+     sleep(0.5)
      puts ""
      puts "To navigate cat breeds, enter 'cats'"
+     sleep(0.5)
      puts ""
      puts "To exit, enter 'exit'"
      puts ""
@@ -44,7 +46,6 @@ EOF
      bye_bye
      else 
      not_an_option
-     continue
      menu
      end
    end
@@ -59,7 +60,7 @@ EOF
     elsif second_input == "n"
       bye_bye
     else
-      not_an_option
+      error
     end
   end
 
@@ -72,7 +73,11 @@ EOF
    puts "Which cat breed are you interested in?"
    input = gets.strip.downcase
    breeds_selection(input)
+  
+   end
+
    #binding.pry
+
    end
 
   def breeds_selection(breed)
@@ -80,23 +85,44 @@ EOF
     c = CatBreeds.find_by_name(breed)
     c.each do |c|
     puts "Name: #{c.name}"
+    sleep(1)
     puts "Origin: #{c.origin}"
+    sleep(1) 
     puts "Temperament #{c.temperament}"
+    sleep(1)
     puts "Description: #{c.description}"
-     puts "Life Span: #{c.life_span}"
-     puts "Affection Level: #{c.affection_level}"
-     puts "Shedding Level: #{c.shedding_level}"
-     puts "Intelligence: #{c.intelligence}"
-     
+    sleep(1)
+    puts "Life Span: #{c.life_span}"
+    sleep(1)
+    puts "Affection Level: #{c.affection_level}"
+    sleep(1)
+    puts "Shedding Level: #{c.shedding_level}"
+    sleep(1)
+    puts "Intelligence: #{c.intelligence}"
+    sleep(1)
+
     end
 
     #binding.pry
-   end
+  end
 
    def not_an_option
-    puts "Not an option, please try again"
+    puts "Oops, that's not an option! Please try again."
     menu
    end
+
+   def error
+    puts "Oops, that's not an option! Please try again."
+    sleep(1)
+    continue
+   end
+
+  #  def error
+  #   puts "Oops, that's not an option! Please check your spelling"
+  #   sleep(3)
+  #   breeds_list
+  #  end
+
 
 def bye_bye
 puts ""
@@ -112,7 +138,9 @@ EOF
 puts ""
 exit 
   end
-end
+
+
+
     
   
 
